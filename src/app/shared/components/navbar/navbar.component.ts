2cd8754a7f46d43f5a1cf65460fcbae4c138aac4
Ramiro <zapedax@gmail.com>
2019-12-13 10:31:34 -0600

    Update to info an icons in navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  @ViewChild('navBurger', {static: false}) navBurger: ElementRef;
+  @ViewChild('navMenu', {static: false}) navMenu: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleNavBar() {
+    this.navBurger.nativeElement.classList.toggle('is-active');
+    this.navMenu.nativeElement.classList.toggle('is-active');
   }
 
 }
